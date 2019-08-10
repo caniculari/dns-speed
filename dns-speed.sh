ad3b5a1000d968cfd0b4d1754c03039f74acf568
Octavio Alvarez <octalgl@alvarezp.org>
2019-08-09 20:03:30 -0500

    dns-speed.sh: use three samples instead of one
    This will take three samples for each DNS server in the list
    and it will show the fastest servers based on the average.
    
    This should protect the results from transient delay pikes
    as well as transient optimal situations.

--- dns-speed.sh
+++ dns-speed.sh
@@ -14,20 +14,27 @@ touch $temp_file
 
 # Test the dns servers listed on the dns-list.txt file
 while read i; do
+    sum=0
+    for n_attempt in $(seq 1 3); do
         dns=`echo $i | awk -F ',' '{print $1}'`
         ip=`echo $i | awk -F ',' '{print $2}'`
         response=`dig +timeout=1 @$ip $name | grep 'Query time' | awk '{print $4}'`
         if [ -z "$response"  ]; then
             echo $dns $ip "not responding"
+            break;
         else
             printf "%s" $dns
             printf "\t%s\t" $ip
             printf "%s msec\n" $response
 
-            echo $response msec $dns $ip  >>$temp_file
+            sum=$((sum + response))
 
 
         fi
+        if [ "$n_attempt" -eq "3" ]; then
+            echo $((sum/3)) msec $dns $ip  >>$temp_file
+        fi
+    done
 done <dns-list.txt
 
 # Print your fastest DNS
